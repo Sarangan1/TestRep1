a14d95e67162058abd38b8e9f515ce2d0a83ba80
Saran Satkunasivam <saran.satkunasivam@purefacts.com>
2018-11-18 17:00:11 -0500

    v1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import "./mytable";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">blah</header>
+        <header className="App-header">appfile</header>
       </div>
     );
   }
---
+++ my-app/src/mytable.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class MyTable extends Component {
+  state = {};
+  render() {
+    return <h1>blahblah</h1>;
+  }
+}
+
+export default MyTable;
